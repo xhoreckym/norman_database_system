c4b1c533c9d75719b3a972491bd42e13b084051a
Martin Klauco <martin@klauco.com>
2025-05-19 10:02:24 +0200

    udpate in projects

--- app/Models/Backend/Project.php
+++ app/Models/Backend/Project.php
@@ -2,18 +2,31 @@
 
 namespace App\Models\Backend;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Project extends Model
 {
     use HasFactory;
 
-    protected $table = 'projects';
-
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'name',
         'abbreviation',
         'description',
     ];
+
+    /**
+     * Get all users for the project.
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'project_user')
+            ->withTimestamps();
+    }
 }
