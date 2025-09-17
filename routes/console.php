ffe23a7e0c1cef89d71f22e45518ea7868215efc
Martin Klauco <martin@klauco.com>
2025-09-17 11:48:26 +0200

    updated console.php

--- routes/console.php
+++ routes/console.php
@@ -4,10 +4,6 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
-
 Schedule::command('telescope:prune --hours=72')->daily();
 
 Schedule::command('logs:clear')->days([1, 5])->at('03:00');
